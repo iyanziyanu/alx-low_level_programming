970626f91ff5438206257116a17627408ab97382
iyanziyanu <iyanziyanu@gmail.com>
2022-12-11 07:40:07 -0800

    print alphabeth

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Putchar alphabeth
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+char i;
+char j; 
+for (i = 'a'; i <= 'z'; i++)
+{
+putchar(i);
+}
+for (j = 'A'; j <= 'Z'; j++)
+{
+putchar(j);
+}
+
+putchar('\n');
+
+return (0);
+}
