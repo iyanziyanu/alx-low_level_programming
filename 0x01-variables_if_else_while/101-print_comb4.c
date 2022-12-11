9f97d7d13f6418f5bdbd32422dd77544e7c86553
iyanziyanu <iyanziyanu@gmail.com>
2022-12-11 13:29:33 -0800

    print comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Putchar alphabeth
+ * main - Print combination of numbersi
  *
  * Return: Always (Success)
  */
@@ -14,6 +14,8 @@ for (digit1 = 0; digit1 < 8; digit1++)
 {
 for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 {
+for (digit3 = digit2 + 1; digit3 < 10; digit3++)
+{
 putchar((digit1 % 10) + '0');
 putchar((digit2 % 10) + '0');
 putchar((digit3 % 10) + '0');
@@ -22,6 +24,7 @@ if (digit1 == 7 && digit2 == 8 && digit3 == 9)
 	continue;
 putchar(',');
 putchar(' ');
+}
 }
 }
 putchar('\n');
