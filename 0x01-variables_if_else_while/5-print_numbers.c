a5a4cf2e829aa59c03f6d747cc1150978e333c20
iyanziyanu <iyanziyanu@gmail.com>
2022-12-11 08:21:17 -0800

    print number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Putchar alphabeth
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+int num;
+for (num = 0; num < 10; num++)
+{
+printf("%d", num);
+}
+printf("\n");
+
+return (0);
+}
