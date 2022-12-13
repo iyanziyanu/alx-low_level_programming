bf711f6dd9ffb780def5a10db65d99dc26865602
iyanziyanu <iyanziyanu@gmail.com>
2022-12-13 11:13:08 -0800

    print alphbethx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabets "a..z"
+ *
+ * Return: Always 0 -> (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+int i;
+for (a = 0; a < 10; a++)
+{
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
+}
