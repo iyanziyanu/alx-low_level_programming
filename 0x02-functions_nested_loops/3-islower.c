15f2067c5ed8c5ea97110321e0a974191b800824
iyanziyanu <iyanziyanu@gmail.com>
2022-12-13 13:10:09 -0800

    islower print

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower - prints 1 if c is lowercase and 0 if otherwise
+ *
+ * @c: char
+ * Return: validations
+ */
+
+int _islower(int c)
+{
+return (c >= 'a' && c <= 'z');
+}
+
