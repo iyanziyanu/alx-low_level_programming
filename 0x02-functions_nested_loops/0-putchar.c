a282eaaaa9ef4b00e93c68a791cd8c26c75a9236
iyanziyanu <iyanziyanu@gmail.com>
2022-12-13 05:26:34 -0800

    prints putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h" /** Custom header file */
+
+/**
+ * main - print "_putchar"
+ *
+ * Return: Always 0 -> (Success)
+ */
+
+int main(void)
+{
+	putchar('');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
