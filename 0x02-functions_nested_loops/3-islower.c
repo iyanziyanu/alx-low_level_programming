2bb78718bac149b60627737c0f53e64def8ac4c3
iyanziyanu <iyanziyanu@gmail.com>
2022-12-14 05:17:20 -0800

    print 1 and 0

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
  * _islower - prints 1 if c is lowercase and 0 if otherwise
- *
  * @c: char
- * Return: validations
+ * Return: Always 0 (Success)
  */
 
 int _islower(int c)
