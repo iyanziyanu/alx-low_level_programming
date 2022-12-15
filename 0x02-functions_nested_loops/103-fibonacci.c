a654e33332bdf3424cd077418a8a889a75fcdbd3
iyanziyanu <iyanziyanu@gmail.com>
2022-12-14 17:27:44 -0800

    print 50

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 /**
- * main - Entry point
+* main - Entry point
  * Description: a program that prints the first 50 characters
  * Return: Allow success
  */
 int main(void)
 {
-long int s = 0, a_1 = 1, a_2 = 2;
-int n;
+unsigned long int  a_1 = 1, a_2 = 2;
+unsigned long  int n;
 
-for (n = 0; a_1 <=  4000000 || a_2 <= 4000000; n++)
+for (n = 0; n < 49; n++)
 {
-if (a_1 % 2 == 0)
-s = s + a_1;
-else if (a_2 % 2 == 0)
-s = s + a_2;
+printf("%lu, %lu", a_1, a_2);
+if (n != 48)
+{
+putchar(',');
+putchar(' ');
+}
 a_1 = a_1 + a_2;
 a_2 = a_1 + a_2;
 }
