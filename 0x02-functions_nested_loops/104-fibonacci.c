898a59cc514cb0c687df75b34c30fb45b9a31998
iyanziyanu <iyanziyanu@gmail.com>
2022-12-14 17:19:39 -0800

    prints 50

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: a program that prints the first 50 characters
+ * Return: Allow success
+ */
+int main(void)
+{
+unsigned long int  a_1 = 1, a_2 = 2;
+unsigned long  int n;
+
+for (n = 0; n < 49; n++)
+{
+printf("%lu, %lu", a_1, a_2);
+if (n != 48)
+{
+putchar(',');
+putchar(' ');
+}
+a_1 = a_1 + a_2;
+a_2 = a_1 + a_2;
+}
+putchar('\n');
+return (0);
+}
