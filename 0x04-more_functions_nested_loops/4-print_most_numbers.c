6d756b950b9e1de0b4f1dc1cbf151d2dce7c4903
iyanziyanu <iyanziyanu@gmail.com>
2023-01-01 13:20:42 -0800

    priny most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Printing numbers between 0 to 9
+ * except 2 and 4, followed by a new line
+ * Return: no return
+ */
+
+void print_most_numbers(void)
+{
+int ch;
+for (ch = 0; ch < 10; ch++)
+{
+if (ch != 2 && ch != 4)
+{
+_putchar(ch + '0');
+}
+}
+_putchar('\n');
+}
